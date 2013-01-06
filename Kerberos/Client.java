3f71c4c196d9dd706980f0e4c55ab341b9ae3e93
Till Theis <theis.till@gmail.com>
2013-01-06 19:19:56 +0100

    validate existence of ticket response

--- Kerberos/Client.java
+++ Kerberos/Client.java
@@ -36,25 +36,31 @@ public class Client extends Object {
 
         boolean success = false;
 
-        if (ticketResponse.decrypt(generateSimpleKeyForPassword(password))) {
-            logGood("decrypted ticket response");
+        if (ticketResponse != null) {
+            logGood("received ticket response");
 
-            if (ticketResponse.getNonce() == nonce) {
-                logGood("validated nonce from response");
+            if (ticketResponse.decrypt(generateSimpleKeyForPassword(password))) {
+                logGood("decrypted ticket response");
 
-                currentUser = userName;
-                tgsSessionKey = ticketResponse.getSessionKey();
-                tgsTicket = ticketResponse.getResponseTicket();
+                if (ticketResponse.getNonce() == nonce) {
+                    logGood("validated nonce from response");
 
-                // remove password from memory
-                Arrays.fill(password, (char) 0);
+                    currentUser = userName;
+                    tgsSessionKey = ticketResponse.getSessionKey();
+                    tgsTicket = ticketResponse.getResponseTicket();
 
-                success = true;
+                    // remove password from memory
+                    Arrays.fill(password, (char) 0);
+
+                    success = true;
+                } else {
+                    logBad("could not validate nonce from response");
+                }
             } else {
-                logBad("could not validate nonce from response");
+                logBad("could not decrypt ticket response");
             }
         } else {
-            logBad("could not decrypt ticket response");
+            logBad("did not receive ticket response (wrong user or server)");
         }
 
         return success;
