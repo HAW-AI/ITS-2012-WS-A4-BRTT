6890dae09b82e53da4fae8b64c6cd773d50c3453
Till Theis <theis.till@gmail.com>
2013-01-06 17:51:12 +0100

    add fundamental files

---
+++ Kerberos/Client.java
@@ -0,0 +1,56 @@
+package Kerberos;
+
+/* Simulation einer Kerberos-Session mit Zugriff auf einen Fileserver
+ /* Client-Klasse
+ */
+
+import java.util.*;
+
+public class Client extends Object {
+
+	private KDC myKDC;
+
+	private Server myFileserver;
+
+	private String currentUser;
+
+	private Ticket tgsTicket = null;
+
+	private long tgsSessionKey; // K(C,TGS)
+
+	private Ticket serverTicket = null;
+
+	private long serverSessionKey; // K(C,S)
+
+	// Konstruktor
+	public Client(KDC kdc, Server server) {
+		myKDC = kdc;
+		myFileserver = server;
+	}
+
+	public boolean login(String userName, char[] password) {
+	// TODO!!
+	}
+
+	public boolean showFile(String serverName, String filePath) {
+	// TODO!!
+	}
+
+	/* *********** Hilfsmethoden **************************** */
+
+	private long generateSimpleKeyForPassword(char[] pw) {
+		// Liefert einen Schlüssel für ein Passwort zurück, hier simuliert als
+		// long-Wert
+		long pwKey = 0;
+		for (int i = 0; i < pw.length; i++) {
+			pwKey = pwKey + pw[i];
+		}
+		return pwKey;
+	}
+
+	private long generateNonce() {
+		// Liefert einen neuen Zufallswert
+		long rand = (long) (100000000 * Math.random());
+		return rand;
+	}
+}
